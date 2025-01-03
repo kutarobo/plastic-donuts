236593c5f8657cf848c019adc21c5de0568ec3c6
kutarobo <kutarobo@naver.com>
2025-01-03 12:47:04 +0900

    env: 린트, 프리티어, 사스 적용

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,23 @@
-import * as React from "react";
-import type { HeadFC, PageProps } from "gatsby";
-import Layout from "../components/layout";
+import * as React from 'react'
+import type { HeadFC, PageProps } from 'gatsby'
+import Layout from '../components/layout'
+import '../styles/global.scss'
+
 const pageStyles = {
-  color: "#232129",
+  color: '#232129',
   padding: 96,
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-};
+  fontFamily: '-apple-system, Roboto, sans-serif, serif',
+}
+
 const IndexPage: React.FC<PageProps> = () => {
   return (
     <Layout>
       <main style={pageStyles}>Test</main>
     </Layout>
-  );
-};
+  )
+}
 
-export default IndexPage;
+export default IndexPage
 
 export const Head: HeadFC = () => {
   return (
@@ -22,5 +25,5 @@ export const Head: HeadFC = () => {
       <title>plastic donuts</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     </>
-  );
-};
+  )
+}
